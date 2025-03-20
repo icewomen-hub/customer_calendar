2f5cc7b88aac2497ea6d2ec9bbdf13073e940834
Sven Schrodt <sven@schrodt.nrw>
2025-03-20 10:25:21 +0100

    Cleaning bot started by PoE

--- src/core/customer_calendar.py
+++ src/core/customer_calendar.py
@@ -35,36 +35,9 @@ class CustomerCalendar:
 
     def edit(self, line):
         work_line = self.data[self.data["id"] == line["id"]]
-        # print(work_line)
-        # print(line)
-        # exit()
         for i in work_line:
             work_line[i] = line[i]
             
         self.data[self.data["id"] == line["id"]] = work_line
 
 
-cc = CustomerCalendar()
-
-
-##cc.book(c_start='2025-05-21 09:30:12', c_end='2025-05-21 10:00:11', appt_id=53, member_id=68)
-# cc.save()
-# w = cc.data[cc.data['id']== 4]
-# w['app_type_id'] = 44
-# äprint(w)
-
-
-to_be_editd = {
-    "id": 4,
-    "start": "2025-12-23 12:11:09",
-    "end": "2025-03-12 20:00:06",
-    "app_type_id": 53,
-    "member_id": 68,
-    "status": "beantragt",
-    "comment": 'Foo bar info',
-    "add_info": 'No milk today',
-}
-# cc.save()}
-cc.edit(to_be_editd)
-print(cc.data)
-cc.save() 
